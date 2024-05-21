8bf04649f237f4aea4fac092f7188663b4fb2a12
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-21 20:45:52 +0000

    created step two

--- app.rb
+++ app.rb
@@ -16,15 +16,30 @@ get("/") do
 end
 
 
-get("/:first_currency") do
+get("/:from_currency") do
   
-  @the_currency = params.fetch("first_currency")
+  @the_currency = params.fetch("from_currency")
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("FX_KEY")}"
+
   @raw_response = HTTP.get(api_url)
   @raw_string = @raw_response.to_s
   @parsed_data = JSON.parse(@raw_string)
   @all_currencies = @parsed_data.fetch("currencies")
   
   erb(:step_one)
+end
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
+
+  @convert_url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}&amount=1&access_key=#{ENV.fetch("FX_KEY")}"
+
+  @raw_response = HTTP.get(@convert_url)
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  @amount = @parsed_response.fetch("result")
+
+  erb(:second_step)
 end
