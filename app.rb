932812a3f8757b8991666b60c88ae5aaf14809b6
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-21 02:12:00 +0000

    created list of currencies

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ get("/") do
   @raw_response = HTTP.get(api_url)
   @raw_string = @raw_response.to_s
   @parsed_data = JSON.parse(@raw_string)
-
+  @all_currencies = @parsed_data.fetch("currencies")
+  
+  
   erb(:homepage)
 
 end
