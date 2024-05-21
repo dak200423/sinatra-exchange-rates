54dfc7c3ad9644f3c431d72be49ba9ca764cef6d
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-21 02:33:20 +0000

    step_one complete

--- app.rb
+++ app.rb
@@ -14,3 +14,17 @@ get("/") do
   erb(:homepage)
 
 end
+
+
+get("/:first_currency") do
+  
+  @the_currency = params.fetch("first_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("FX_KEY")}"
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @all_currencies = @parsed_data.fetch("currencies")
+  
+  erb(:step_one)
+end
